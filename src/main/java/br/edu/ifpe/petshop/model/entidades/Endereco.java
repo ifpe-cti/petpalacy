a88f2e4bc14821a1ee43f71ac1fbecea7addccdc
wemersonifpe <@wemersonifpe>
2018-03-24 22:12:09 -0300

    criação da clase endereco

--- src/main/java/br/edu/ifpe/petshop/model/entidades/Endereco.java
+++ src/main/java/br/edu/ifpe/petshop/model/entidades/Endereco.java
@@ -10,5 +10,92 @@ package br.edu.ifpe.petshop.model.entidades;
  * @author Aluno
  */
 public class Endereco {
+    private int id;
+    private String rua;
+    private String cidade;
+    private String bairro;
+    private String cep;
+
+    public Endereco() {
+    }
+
+    public Endereco(int id, String rua, String cidade, String bairro, String cep) {
+        this.id = id;
+        this.rua = rua;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.cep = cep;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" + "id=" + id + ", rua=" + rua + ", cidade=" + cidade + ", bairro=" + bairro + ", cep=" + cep + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 31 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Endereco other = (Endereco) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
