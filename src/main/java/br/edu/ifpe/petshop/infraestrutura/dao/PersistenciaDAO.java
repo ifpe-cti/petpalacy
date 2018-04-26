10fd8167fbef8a659b4e63d07e7ab69eb1878777
Kaio-Brasil <kaio.gus@outlook.com>
2018-04-26 19:54:08 -0300

    #26 Atualização.

--- src/main/java/br/edu/ifpe/petshop/infraestrutura/dao/PersistenciaDAO.java
+++ src/main/java/br/edu/ifpe/petshop/infraestrutura/dao/PersistenciaDAO.java
@@ -1,26 +1,25 @@
-/*
-MIT License
+/*MIT License
 
-Copyright (c) 2018 Daniel da Silva Calado, Izaquiel Cavalcante da Silva, 
-                   Kaio Cesar Bezerra da Silva e Wemerson Diogenes da Silva
+ Copyright (c) 2018 Daniel da Silva Calado, Izaquiel Cavalcante da Silva, 
+                   Kaio Cesar Bezerra da Silva e Wemerson Diogenes da Silva
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
  */
 package br.edu.ifpe.petshop.infraestrutura.dao;
 
@@ -71,16 +70,17 @@ public class PersistenciaDAO {
 
     public List listar(String sql) {
         Session session = this.sessionFactory.openSession();
-        List<Object> lista = null;
+        List lista = null;
         
         try {
             Query consulta = session.createQuery(sql);
-            //List<Object> lista = (List<Object>) consulta.list().get(0);
+            lista = (List) consulta.list();
         } catch(RuntimeException ex) {
             throw ex;
         } finally {
                 session.close();
         }
+        
         return lista;
         
     }
