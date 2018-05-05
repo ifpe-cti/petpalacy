a5ecb0c5ff851a9500ac5ddb00fa245c08c99315
20161d13gr0031 <20161d13gr0031@GAR-103526.labs.garanhuns.ifpe>
2018-05-04 21:45:58 -0300

    #45 troca do nome do rewpositorio para petpalacy

---
+++ src/main/java/br/edu/ifpe/petpalacy/Teste.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.petpalacy;
+
+import br.edu.ifpe.petshop.model.entidades.Agendamento;
+import br.edu.ifpe.petshop.model.entidades.Cliente;
+import br.edu.ifpe.petshop.model.entidades.DatasServico;
+import br.edu.ifpe.petshop.model.entidades.Empresa;
+import br.edu.ifpe.petshop.model.entidades.Endereco;
+import br.edu.ifpe.petshop.model.entidades.Servico;
+import br.edu.ifpe.petshop.model.repositorio.implementacao.RepositorioAgendamentoImpDB;
+import br.edu.ifpe.petshop.model.repositorio.implementacao.RepositorioClienteImpDB;
+import br.edu.ifpe.petshop.model.repositorio.implementacao.RepositorioEmpresaImpDB;
+import br.edu.ifpe.petshop.model.repositorio.implementacao.RepositorioServicoImpDB;
+import br.edu.ifpe.petshop.model.repositorio.interfaces.RepositorioServico;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Jose Junio
+ */
+public class Teste {
+    public static void main(String[] args) {
+
+RepositorioClienteImpDB impb = new RepositorioClienteImpDB();
+impb.buscar(1);
+      /*  Endereco end = new Endereco(0, "logradouro", "cidade", "bairro");
+        Cliente cli = new Cliente(0, "nome", "cpf", "telefone", end, "email", "senha");
+        impb.salvar(cli);
+        
+        RepositorioServico SER  = new RepositorioServicoImpDB();
+        DatasServico ds = new DatasServico(null, new Date(), "status");
+        ArrayList aa = new ArrayList();
+        aa.add(ds);
+        Servico s = new Servico(0, "nome", 0, null, aa);
+        SER.salvar(s);
+        ArrayList ss = new ArrayList();
+        ss.add(s);
+        
+        RepositorioEmpresaImpDB emp = new RepositorioEmpresaImpDB();
+        Empresa em = new Empresa("cnpj", "s", "r", "d", "d", ss, end);
+        emp.salvar(em);
+        
+        RepositorioAgendamentoImpDB arg = new RepositorioAgendamentoImpDB();
+        Agendamento ag  = new Agendamento(0, ss, cli, em, new Date(), null);
+        arg.salvar(ag);
+        */
+      
+    }
+}
---
+++ src/main/java/br/edu/ifpe/petpalacy/model/entidades/Endereco.java
@@ -0,0 +1,116 @@
+/*
+MIT License
+
+Copyright (c) 2018 Daniel da Silva Calado, Izaquiel Cavalcante da Silva, 
+                   Kaio Cesar Bezerra da Silva e Wemerson Diogenes da Silva
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package br.edu.ifpe.petpalacy.model.entidades;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Wemerson Diogenes da Silva <wemersondiogenes16@gmail.com>
+ */
+@Entity
+public class Endereco implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String logradouro;
+    private String cidade;
+    private String bairro;
+
+    public Endereco() {
+    }
+
+    public Endereco(int id, String logradouro, String cidade, String bairro) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.cidade = cidade;
+        this.bairro = bairro;
+    }
+
+    public int getIdEndereco() {
+        return id;
+    }
+
+    public void setIdEndereco(int id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Endereco)) {
+            return false;
+        }
+        Endereco endereco = (Endereco) obj;
+
+        if(endereco.id == id
+                && endereco.bairro == bairro
+                && endereco.cidade == cidade
+                && endereco.logradouro == logradouro) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" + "id=" + id + ", logradouro=" + logradouro + ", cidade=" + cidade + ", bairro=" + bairro + '}';
+    }
+
+}
