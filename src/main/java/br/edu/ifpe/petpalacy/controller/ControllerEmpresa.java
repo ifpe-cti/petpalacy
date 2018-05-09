bde03581cc63e8e5e8cc7c9f6c0aa7966175091a
danielcalado <danielcalado159@gmail.com>
2018-05-09 16:39:09 -0300

    #54 Criação das classes controllerCliente e controllerEmpresa

---
+++ src/main/java/br/edu/ifpe/petpalacy/controller/ControllerEmpresa.java
@@ -0,0 +1,70 @@
+/*
+MIT License
+
+Copyright (c) 2018 Daniel da Silva Calado, Izaquiel Cavalcante da Silva, 
+                   Kaio Cesar Bezerra da Silva e Wemerson Diogenes da Silva
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package br.edu.ifpe.petpalacy.controller;
+
+import br.edu.ifpe.petpalacy.model.entidades.Empresa;
+import br.edu.ifpe.petpalacy.model.negocio.NegocioEmpresa;
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Daniel Calado <danielcalado159@gmail.com>
+ */
+@ManagedBean
+@SessionScoped
+public class ControllerEmpresa implements Serializable{
+    private NegocioEmpresa negEmpresa;
+    private Empresa empresa;
+    private ArrayList<Empresa> listaEmpresa;
+
+    public ControllerEmpresa() {
+        negEmpresa = new NegocioEmpresa();
+        empresa = new Empresa();
+        listaEmpresa = new ArrayList<>();
+    }
+    
+    public void salvar(){
+        negEmpresa.salvar(empresa);
+    }
+    public void alterar(){
+        negEmpresa.editar(empresa);
+    }
+    public void deletar(){
+        negEmpresa.deletar(empresa);
+    }
+    public void listar(){
+       listaEmpresa = (ArrayList<Empresa>) negEmpresa.listar();
+    }
+    public void buscarId(){
+        negEmpresa.buscar(empresa.getIdEmpresa());
+    }
+    public void bu8scarCnpj(){
+        negEmpresa.buscarCnpj(empresa.getCnpj());
+    }
+
+}
