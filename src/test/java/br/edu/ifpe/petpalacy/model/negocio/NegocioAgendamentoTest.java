fcd0aca1fe9acd164253187b6849ced2576c212a
izaquiel7 <lotygun7@gmail.com>
2018-07-06 21:04:47 -0300

    #107 NegocioAgendamento, 9 testes 100%

--- src/test/java/br/edu/ifpe/petpalacy/model/negocio/NegocioAgendamentoTest.java
+++ src/test/java/br/edu/ifpe/petpalacy/model/negocio/NegocioAgendamentoTest.java
@@ -53,125 +53,126 @@ public class NegocioAgendamentoTest {
     private static Endereco endereco;
     private static Empresa empresa;
     private static Cliente cliente;
-    private static Servico serve;
-    private static Agendamento agenda;
-    private static NegocioAgendamento negAgen;
-    private ArrayList<Agendamento> list = null;
+    private static Servico servico;
+    private static Agendamento agendamento;
+    private static NegocioAgendamento negocioAgendamento;
+    private ArrayList<Agendamento> agendamentos = null;
 
-    private static NegocioCliente negCli;
-    private static NegocioEmpresa negEmp;
-    private static NegocioServico negServe;
-    private Agendamento altAgen;
+    private static NegocioCliente negocioCliente;
+    private static NegocioEmpresa negocioEmpresa;
+    private static NegocioServico negocioServico;
+    private Agendamento agendamentoAlterado;
 
     public NegocioAgendamentoTest() {
-        negAgen = new NegocioAgendamento();
-        list = new ArrayList<>();
+        negocioAgendamento = new NegocioAgendamento();
+        agendamentos = new ArrayList<>();
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
 
-        serve = new Servico("Limpa ku", 4, null, empresa);
+        servico = new Servico("Limpa cao", 4, null, empresa);
         cliente = new Cliente("Carlos junio", "11419189433", "879812324", endereco, "dkpaz@gmail.com", "saosap");
         empresa = new Empresa("12602190000104", "papapa", "emp@gmail.com", "21212121", "bruno Eletro", endereco);
         endereco = new Endereco("rua do Parque", 145, "sao joao", "la em cima");
-        agenda = new Agendamento(serve, cliente, empresa, new Date(), new Date(), new BigDecimal(35), StatusAgen.ESPERA);
+        agendamento = new Agendamento(servico, cliente, empresa, new Date(), new Date(), new BigDecimal(35), StatusAgen.ESPERA);
 
-        negCli = new NegocioCliente();
-        negEmp = new NegocioEmpresa();
-        negServe = new NegocioServico();
+        negocioCliente = new NegocioCliente();
+        negocioEmpresa = new NegocioEmpresa();
+        negocioServico = new NegocioServico();
 
-        negCli.salvar(cliente);
-        negEmp.salvar(empresa);
-        negServe.salvar(serve);
+        negocioCliente.salvar(cliente);
+        negocioEmpresa.salvar(empresa);
+        negocioServico.salvar(servico);
 
     }
 
     @Before
     public void setUp() throws Exception {
-        negAgen.salvar(agenda);
-
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-
-        negAgen.deletar(agenda);
-        negServe.deletar(serve);
-        negCli.deletar(cliente);
-        negEmp.deletar(empresa);
+        negocioAgendamento.salvar(agendamento);
 
     }
 
     /**
+     * @AfterClass public static void tearDownClass() throws Exception {
+     * negServe.deletar(serve); negCli.deletar(cliente);
+     * negEmp.deletar(empresa); negAgen.deletar(agenda);
+     *
+     *
+     * }
+     *
+     *
      * Test of salvar method, of class NegocioAgendamento.
      *
      * @throws java.lang.Exception
      */
     @Test
-    public void test1SalvarAgendamentoNoBanco() throws Exception {
+    public void test1DeveSalvarAgendamentoNoBanco() throws Exception {
 
-        list = (ArrayList) negAgen.listar();
-
-        assertEquals("11419189433", list.get(0).getCliente().getCpf());
+        assertEquals("11419189433", agendamento.getCliente().getCpf());
 
     }
 
     @Test(expected = java.lang.Exception.class)
-    public void test2ErroAoSalvarNoBanco() throws Exception {
-        negAgen.salvar(null);
+    public void test2DeveDarErroAoSalvar() throws Exception {
+        negocioAgendamento.salvar(null);
 
     }
 
     @Test
-    public void test3AterarAgendamento() throws Exception {
-        altAgen = new Agendamento(new Servico("toza", 3, new BigDecimal(50), empresa), cliente, empresa, new Date(), new Date(), new BigDecimal(50), StatusAgen.ESPERA);
-        negServe.editar(new Servico("toza", 3, new BigDecimal(50), empresa));
-        negAgen.editar(altAgen);
+    public void test3DeveAterarAgendamentoNoBanco() throws Exception {
+        servico.setNome("toza");
+        agendamento.setServico(servico);
+        negocioAgendamento.editar(agendamento);
 
-        list = (ArrayList) negAgen.listar();
-
-        for (Agendamento a : list) {
-            if (!a.getServico().getNome().equals("Limpa ku")) {
-                assertEquals("toza", a.getServico().getNome());
-            }
-        }
+        assertEquals("toza", agendamento.getServico().getNome());
 
     }
 
     @Test(expected = java.lang.Exception.class)
-    public void test4ErroAoAlterar() throws Exception {
+    public void test4DeveDarErroAoAlterar() throws Exception {
+        negocioAgendamento.salvar(agendamento);
+        agendamentoAlterado = new Agendamento(new Servico("toza", 3, new BigDecimal(50), empresa), null, empresa, new Date(), new Date(), new BigDecimal(50), StatusAgen.ESPERA);
 
-        altAgen = new Agendamento(new Servico("toza", 3, new BigDecimal(50), empresa), null, empresa, new Date(), new Date(), new BigDecimal(50), StatusAgen.ESPERA);
-
-        negAgen.editar(altAgen);
+        negocioAgendamento.editar(agendamentoAlterado);
     }
 
-    public void test5DeletarAgendamentoNoBanco() throws Exception {
+    @Test
+    public void test5DeveDeletarAgendamentoDoBanco() throws Exception {
+        negocioAgendamento.salvar(agendamento);
+        negocioAgendamento.deletar(agendamento);
 
-        negAgen.deletar(altAgen);
-        
-        list = (ArrayList) negAgen.listar();
+        agendamentos = (ArrayList) negocioAgendamento.listar();
 
-        assertEquals(null, list.get(0));
+        assertNull(null, negocioAgendamento.buscar(agendamento.getId()));
 
     }
 
     @Test(expected = java.lang.Exception.class)
     public void test6ErroAoDeletar() throws Exception {
 
-        negAgen.deletar(null);
+        negocioAgendamento.deletar(null);
 
     }
 
     @Test
-    public void test7ListarAgendamentos() throws Exception {
-        list = (ArrayList) negAgen.listar();
+    public void test7DeveListarAgendamentos() throws Exception {
+        agendamentos = (ArrayList) negocioAgendamento.listar();
 
-        int cont = 0;
-        for (Agendamento a : list) {
-            cont++;
-        }
-        assertNotEquals(0, cont);
+        assertNotEquals(0, agendamentos.size());
+    }
+
+    @Test
+    public void test8DeveBuscarAgendamentoNoBanco() throws Exception {
+        negocioAgendamento.salvar(agendamento);
+        Agendamento a = negocioAgendamento.buscar(agendamento.getId());
+
+        assertNotNull(a);
+    }
+
+    @Test
+    public void test9DeveRetornarNullAoBuscarAgendamento() throws Exception {
+
+        assertNull(negocioAgendamento.buscar(null));
     }
 }
